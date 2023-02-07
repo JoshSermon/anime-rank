9de9184751d7a984417c64e511ee4a6b75bbc8b4
Josh Sermon <jsermon@LuckieATL4145.attlocal.net>
2023-02-06 23:44:10 -0500

    Added background and nav

---
+++ anime-rank/src/Body.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import * as React from 'react';
+
+  
+  
+  export default class Body extends React.Component<{}> {
+    public render() {
+      return (
+        <div>
+         
+        </div>
+      );
+    }
+  }
